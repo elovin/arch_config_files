7d118be94bb1e9c2c5427cad18c06e71949dd053
elovin <dontthinkyouknowme@gmail.com>
2020-05-11 09:25:49 +0200

    Update pre-package-mod-lvm-snapshot.sh

--- pacman_hooks_files/pre-package-mod-lvm-snapshot.sh
+++ pacman_hooks_files/pre-package-mod-lvm-snapshot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # lvm(8) man page:
 # On invocation, lvm requires that only the standard file descriptors stdin, stdout
 # and stderr are available. If others are found, they get closed and messages
@@ -8,34 +9,40 @@
 # disabling the warnings for now as they dont seem dangerous
 export LVM_SUPPRESS_FD_WARNINGS=1
 
-number_of_current_backups="$(lvs --all | awk '{print $1}' | grep 'backup_*' | wc -l)"
 
-if [ "$number_of_current_backups" -gt 3 ]
+#configure the number of concurrent backups
+maximum_number_of_concurrent_backups=3
+number_of_current_backups="$(lvs | awk '{print $1}' | grep 'backup_*' | wc -l)"
+
+
+if [ "$number_of_current_backups" -ge "$maximum_number_of_concurrent_backups" ]
 then
-        number=1
 
         for backup_name in $(lvs | grep "backup_*" | awk '{print $1}')
         do
-
-                if [ $((number+3)) -gt "$number_of_current_backups" ]
+                if [ "$number_of_current_backups" -lt "$maximum_number_of_concurrent_backups" ]
                 then
                         break
                 fi
 
-                lvremove --yes "/dev/archBaseGroup/$backup_name"
-                number=$((number+1))
+                lvremove --yes "/dev/[VOLUME_GROUP_NAME]/$backup_name"
+                number_of_current_backups="$(lvs | awk '{print $1}' | grep 'backup_*' | wc -l)"
         done
 fi
 
+
 unset backup_name
 
-for backup_name in $(lvs | grep "backup_*" | grep "100.00" | awk '{print $1}')
 
-do
-        lvremove --yes "/dev/archBaseGroup/$backup_name"
+# also remove snapshots without remaning space
+for backup_name in $(lvs | grep "backup_*" | grep "100.00" | awk '{print $1}')
+do 
+        lvremove --yes "/dev/[VOLUME_GROUP_NAME]/$backup_name"
 done
 
 
-current_date=`date +"%Y-%m-%d-%H-%M-%S"`
 
-lvcreate -L5G -s -n backup_"$current_date" /dev/archBaseGroup/root
+# create new snapshot of root partition with current date as name
+# consider moving /var to a different lvm volume so that snapshots grow slower
+current_date=`date +"%Y-%m-%d-%H-%M-%S"`
+lvcreate -L5G -s -n backup_"$current_date" /dev/[VOLUME_GROUP_NAME]/root
